2ce219654dd5a7588142161188d9b375c482a594
francismelojr <francismelo.jr@gmail.com>
2024-07-28 17:58:46 -0300

    corrected schedule interval

--- dags/api_extractor.py
+++ dags/api_extractor.py
@@ -55,7 +55,7 @@ def landing_to_bronze():
 
 dag = DAG('etl_pipeline',
           default_args=default_args,
-          schedule_interval='"0 8 * * *"')
+          schedule_interval="0 8 * * *")
 
 
 collect_data = PythonOperator(
